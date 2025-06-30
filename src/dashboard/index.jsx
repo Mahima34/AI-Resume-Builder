027631680f3e4990bfb0e2392e0b5f7b84b12a66
Mahima Rajput <135412470+Mahima34@users.noreply.github.com>
2025-06-30 12:12:24 +0530

    update

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 function Dashboard() {
   return (
-    <div>
-      Dashboard
+    <div className='p-10 md:px-20 lg:px-32 '>
+      <h2 className='font-bold text-3xl'>My Resume</h2>
+      <p>Start Creating AI Resume To Your Next Job Role</p>
     </div>
   )
 }
